031392e71b8e7611d9b5e14cecd90bb1346a78d4
xrombik <xrombik@yandex.ru>
2021-04-20 22:50:08 +0300

    Update configure-iface.sh

--- configure-iface.sh
+++ configure-iface.sh
@@ -7,5 +7,5 @@ IFACE=lo
 #ip link set dev ${IFACE} down
 #ip link set dev ${IFACE} address 70:4d:7b:65:2a:d0
 #ip link set dev ${IFACE} up
-ifconfig ${IFACE}:10 172.16.1.10 netmask 255.255.255.0
-ifconfig ${IFACE}:30 172.16.1.30 netmask 255.255.255.0
+ifconfig ${IFACE}:10 172.16.2.10 netmask 255.255.255.0
+ifconfig ${IFACE}:30 172.16.2.30 netmask 255.255.255.0
