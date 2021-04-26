a1e3b3277f66e891f614701dea5f7e58c5e1acda
sasha80 <sasha@mail.ru>
2021-04-26 16:35:22 +0300

    WIP: Добавление UDP

--- deconf-iface.sh
+++ deconf-iface.sh
@@ -7,7 +7,7 @@ IFACE=lo
 #DEF_MAC=$(ethtool -P ${IFACE} | grep -iEom1 '([0-9a-f]{2}:){5}[0-9a-f]{2}')
 
 ip addr flush dev ${IFACE}:30
-ip addr flush dev ${IFACE}:30
+ip addr flush dev ${IFACE}:10
 #ip link set dev ${IFACE} down
 #ip link set dev ${IFACE} address ${DEF_MAC}
 #ip link set dev ${IFACE} up
---
+++ gen-udp-test-data.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+import socket
+import argparse
+
+"""
+\file
+\brief Генерирует данные харктерные для udp-протокола
+"""
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-port',     default=50090,         help='Порт назначения')
+    parser.add_argument('-selfaddr', default='172.16.1.10', help='Свой адрес')
+    parser.add_argument('-extaddr',  default='172.16.1.30', help='Внешний адрес')
+    parser.add_argument('-data',     default='1234567890',  help='Порт назначения')
+    opts, opts_unk = parser.parse_known_args()
+    for opt in opts_unk:
+        print(u'внимание: неизвестный аргумент: \"%s\"' % opt)
+
+    self_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    self_s.bind((opts.selfaddr, opts.port))
+
+    ext_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ext_s.bind((opts.extaddr, opts.port))
+
+    ext_s.sendto(opts.data, (opts.selfaddr, opts.port))
+
+    data = self_s.recv(1500)
+    self_s.sendto(data, (opts.extaddr, opts.port))
+
+
+if __name__ == '__main__':
+    main()
